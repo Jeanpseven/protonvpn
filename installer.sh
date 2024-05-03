037ab20af83a4c4405ced88c20d2e94d090d274e
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2024-05-03 18:18:02 +0000

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-wget https://repo2.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.3-3_all.deb
-sudo dpkg -i ./protonvpn-stable-release_1.0.3-3_all.deb && sudo apt update
-sudo apt install proton-vpn-gnome-desktop
-
 # Verificar se o ProtonVPN está instalado
 if dpkg -l | grep -q "protonvpn-gui"; then
     echo "ProtonVPN já está instalado."
 else
     echo "ProtonVPN não está instalado. Instalando..."
+    wget https://repo2.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.3-3_all.deb
+    sudo dpkg -i ./protonvpn-stable-release_1.0.3-3_all.deb && sudo apt update
+    sudo apt install proton-vpn-gnome-desktop
     sudo apt update
     sudo apt install protonvpn-gui
     sudo apt install proton-vpn-gnome-desktop
+    sudo apt install libayatana-appindicator3-1 gir1.2-ayatanaappindicator3-0.1 gnome-shell-extension-appindicator
 fi
