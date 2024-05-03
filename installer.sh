f2feba86373951c3fb068ee52fa97649d0e1edfa
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2024-05-03 18:29:56 +0000

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -6,10 +6,18 @@ if dpkg -l | grep -q "protonvpn-gui"; then
 else
     echo "ProtonVPN não está instalado. Instalando..."
     wget https://repo2.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.3-3_all.deb
-    sudo dpkg -i ./protonvpn-stable-release_1.0.3-3_all.deb && sudo apt update
-    sudo apt install proton-vpn-gnome-desktop
+    sudo dpkg -i ./protonvpn-stable-release_1.0.3-3_all.deb
     sudo apt update
-    sudo apt install protonvpn-gui
     sudo apt install proton-vpn-gnome-desktop
+    sudo apt install protonvpn-gui
     sudo apt install libayatana-appindicator3-1 gir1.2-ayatanaappindicator3-0.1 gnome-shell-extension-appindicator
+fi
+
+# Verifica se a variável DBUS_SESSION_BUS_ADDRESS está definida
+if [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
+    # Se não estiver definida, executa dbus-launch e exporta o resultado
+    eval $(dbus-launch)
+    echo "DBUS_SESSION_BUS_ADDRESS definida como: $DBUS_SESSION_BUS_ADDRESS"
+else
+    echo "DBUS_SESSION_BUS_ADDRESS já está definida como: $DBUS_SESSION_BUS_ADDRESS"
 fi
